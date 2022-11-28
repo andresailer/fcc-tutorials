8e77edab0cdf725bfcf10f313ce044c3b54e0cea
Andre Sailer <andre.philippe.sailer@cern.ch>
2022-11-28 10:47:34 +0100

    geometryTutorial: Update showPlots to histoprint

--- full-detector-simulations/Geometry/showPlots.py
+++ full-detector-simulations/Geometry/showPlots.py
@@ -1,19 +1,18 @@
 #!/bin/env python
-import sys, ROOT, uproot3 as uproot
-from ROOT import TFile, TTree
+import sys, ROOT
+from histoprint import print_hist
+
 ROOT.gROOT.SetBatch(True)
+
 if len(sys.argv) < 2:
-  print("Please specify input file"); sys.exit(1)
+    print("Please specify input file"); sys.exit(1)
 inputFile = sys.argv[1]; print("Reading:", inputFile)
+
 tfile = ROOT.TFile.Open(inputFile)
 myTree = tfile.Get("events")
 myTree.Draw("ECalEndcapCollection.position.z>>zHist(100, 2300, 2510)",
-"ECalEndcapCollection.position.z > 0")
+            "ECalEndcapCollection.position.z > 0")
 myTree.Draw("ECalEndcapCollection.energy>>eHist(30, 0, 0.002)")
-zHist = tfile.Get("zHist")
-eHist = tfile.Get("eHist")
-oFile = ROOT.TFile.Open("temp.root", "RECREATE")
-eHist.Write(); zHist.Write(); oFile.Close()
-uFile = uproot.open("temp.root")
-uFile["zHist"].show()
-uFile["eHist"].show()
+
+print_hist(tfile.Get("zHist"), title="Hits per Layer")
+print_hist(tfile.Get("eHist"), title="Energy per Hit")
